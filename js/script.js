f23ec025482b2876519b46f2c87bb3291e515a93
mrrred <zarubinr85@gmail.com>
2025-04-14 00:03:49 +0700

    Подправил баги с версией на телефоне

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
-// Меню для телефона
-document.querySelector('.menu-toggle').addEventListener('click', function() {
+document.querySelector('.menu-toggle').addEventListener('click', function(e) {
+    e.stopPropagation();
     document.querySelector('.navigation').classList.toggle('active');
 });
 
-// Срытие меню, при клике в любую другую точку экрана
 document.addEventListener('click', (e) => {
     if (!e.target.closest('.navigation') && !e.target.closest('.menu-toggle')) {
-    document.querySelector('.navigation').classList.remove('active');
+        document.querySelector('.navigation').classList.remove('active');
     }
-});
-
-// Плавная прокрутка к якорям
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-    });
-    });
 });
